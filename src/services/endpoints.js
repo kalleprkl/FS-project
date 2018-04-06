1f913846c4b687d3448bc4348e25e1df6a9f492b
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-04-06 11:27:30 +0300

    hups

--- src/services/endpoints.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const get = async (url) => {
-    const response = await axios.get(url)
-    console.log(response.data.data.children)
-    return response.data.data.children
-}
-
-export default { get }
